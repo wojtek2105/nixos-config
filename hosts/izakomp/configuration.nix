6e87f057b1b74a63e8d64cff140981ddf3bbc475
Wojtek <wojtek@noreply.gitea.wardyn.dev>
2026-09-12 20:54:41 +0200

    Fix Iza XKB option path

--- hosts/izakomp/configuration.nix
+++ hosts/izakomp/configuration.nix
@@ -7,5 +7,5 @@
   ];
 
   # Menu działa jako AltGr (trzeci poziom) dla polskich znaków w układzie `pl`.
-  xserver.xkb.options = "lv3:menu_switch";
+  services.xserver.xkb.options = "lv3:menu_switch";
 }
